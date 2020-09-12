8b6f7bcb49d160b700f985391cf7e39006206b21
Gustavo Sanchez Galarza <gustavosanchezgalarza@gmail.com>
2020-09-12 13:31:13 -0500

    Adding second svg for fixing share button colors on click

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
 const shareButton  = document.querySelector(".social__share");
 const shareBox = document.querySelector(".social-box");
+const shareImg = document.querySelector(".share__image");
 
 shareButton.addEventListener("click", () => {
   if(shareBox.classList.contains("hidden")){
     shareBox.classList.remove("hidden");
     shareButton.style.backgroundColor  = "hsl(214, 17%, 51%)";
+    shareImg.setAttribute("src","images/icon-share-light.svg");
   }
   else {
     shareBox.classList.add("hidden");
     shareButton.style.backgroundColor  = "#ecf2f8";
+    shareImg.setAttribute("src","images/icon-share.svg");
   }
 });
