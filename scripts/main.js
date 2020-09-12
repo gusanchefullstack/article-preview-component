bda3dfa5e643091ac382974c62e334e287ea2d80
Gustavo Sanchez Galarza <gustavosanchezgalarza@gmail.com>
2020-09-12 13:08:40 -0500

    Initial adjustments to share button changing colors

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,10 @@ const shareBox = document.querySelector(".social-box");
 shareButton.addEventListener("click", () => {
   if(shareBox.classList.contains("hidden")){
     shareBox.classList.remove("hidden");
+    shareButton.style.backgroundColor  = "hsl(214, 17%, 51%)";
   }
   else {
     shareBox.classList.add("hidden");
+    shareButton.style.backgroundColor  = "#ecf2f8";
   }
 });
