ee269a87c6cc8f2926c537bcd4cab36518ef312b
Gustavo Sanchez Galarza <gustavosanchezgalarza@gmail.com>
2020-09-13 18:42:40 -0500

    Javascript modified with event listeners

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const shareButton  = document.querySelector(".social__share");
-const shareBox = document.querySelector(".social-box");
+const shareButtonMobile  = document.querySelector(".social__share__container");
+const shareBox = document.querySelector(".social__box");
 const shareImg = document.querySelector(".share__image");
 
 shareButton.addEventListener("click", () => {
@@ -14,3 +15,17 @@ shareButton.addEventListener("click", () => {
     shareImg.setAttribute("src","images/icon-share.svg");
   }
 });
+
+shareButtonMobile.addEventListener("click", () => {
+  if(shareBox.classList.contains("hidden")){
+    shareBox.classList.remove("hidden");
+    // shareButton.style.backgroundColor  = "hsl(214, 17%, 51%)";
+    // shareImg.setAttribute("src","images/icon-share-light.svg");
+  }
+  else {
+    shareBox.classList.add("hidden");
+    // shareButton.style.backgroundColor  = "#ecf2f8";
+    // shareImg.setAttribute("src","images/icon-share.svg");
+  }
+});
+
