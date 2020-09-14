73ffd6df5f7c1f940f92b0d1d24d96ddf9eb3762
Gustavo Sanchez Galarza <gustavosanchezgalarza@gmail.com>
2020-09-14 10:41:01 -0500

    Adjustmens to click button colors in desktop and mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
-const shareButton  = document.querySelector(".social__share");
+const shareButton  = document.querySelector(".social__icon__share");
 const shareButtonMobile  = document.querySelector(".social__share__container");
 const shareBox = document.querySelector(".social__box");
 const shareImg = document.querySelector(".share__image");
+
 
 shareButton.addEventListener("click", () => {
   if(shareBox.classList.contains("hidden")){
@@ -19,13 +20,12 @@ shareButton.addEventListener("click", () => {
 shareButtonMobile.addEventListener("click", () => {
   if(shareBox.classList.contains("hidden")){
     shareBox.classList.remove("hidden");
-    // shareButton.style.backgroundColor  = "hsl(214, 17%, 51%)";
-    // shareImg.setAttribute("src","images/icon-share-light.svg");
+    
   }
   else {
     shareBox.classList.add("hidden");
-    // shareButton.style.backgroundColor  = "#ecf2f8";
-    // shareImg.setAttribute("src","images/icon-share.svg");
+    shareImg.setAttribute("src","images/icon-share.svg");
+    shareButton.style.backgroundColor = "#ecf2f8";
   }
 });
 
